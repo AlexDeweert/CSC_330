691d3d1dbd9bb4bf4e8e786eb26b6daf4775d35f
AlexDeweert <alexander.deweert@gmail.com>
2018-10-23 13:02:43 -0700

    question 7 big progress still needs to be tested

--- hw4.rkt
+++ hw4.rkt
@@ -58,17 +58,29 @@
 
 ; 6) cat-then-dog
 (define cat-then-dog (
-   letrec( [ f (lambda(x) (cons x (lambda() (f (if (string=? x "cat.jpg") "dog.jpg" "cat.jpg" )) )))])
+   letrec( [ f (lambda(x) (cons x (lambda() (f (if (string=? x "cat.jpg") "dog.jpg" "cat.jpg" )))  )  )])
    (lambda() (f "cat.jpg"))
   )
 )
 
 ; try to merge two streams (zip per Ben)
 ; 7) stream-add-zero
-(define (stream-add-zero s)
-   (letrec( [ return-stream (lambda(x) (stream-add-zero (cdr s)))]))
-   (cons ((cons 0 (car s)))return-stream)
-)
+;(define (stream-add-zero s)
+;)
+
+;stream with zeros
+(define zeros ( lambda() (cons 0 zeros) ))
+
+;want to return a pair of pairs ( (car1 . car2) . ( procedure1 . procedure2 ) )
+;want  pair of the stream cars
+;want a pair of the stream cdrs
+
+;A STREAM IS A THUNK that when called produces a pair of (1) the first element in the sequence and (2) a THUNK that represents the stream for the second-infty elements
+;(define (stream-add-zero s) (letrec([ f( lambda(x,y) (cons  (cons x y)  (lambda())   ) ])) )
+
+;this returns exactly the same thing as (cat-then-dog) if we call (returnstream cat-then-dog)
+;except for the very first element, which is the pair 0 . "cat.jpg" (if called with cat then dog
+(define (returnstream s) (cons (cons (car (zeros)) (car (s))) (cdr (s))))
 
 
 
